49e76555b43d137eb5f98170b6405f2efc849ef0
Ahmed Zein <120061135+ahmedzein74@users.noreply.github.com>
2024-10-31 00:23:17 +0300

    1

--- lib/features/on_boarding/widgets/get_started_button.dart
+++ lib/features/on_boarding/widgets/get_started_button.dart
@@ -1,7 +1,9 @@
+import 'package:appointment_app/core/routing/app_router.dart';
 import 'package:appointment_app/core/theming/colors.dart';
 import 'package:appointment_app/core/theming/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class GetStartedButton extends StatelessWidget {
   const GetStartedButton({super.key});
@@ -16,7 +18,9 @@ class GetStartedButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
         )),
       ),
-      onPressed: () {},
+      onPressed: () {
+        context.pushReplacement(AppRouter.loginScreen);
+      },
       child: Text(
         'Get Started',
         style: TextStyles.font16WhiteSemibold,
